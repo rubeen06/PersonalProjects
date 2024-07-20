d44159bc9b7526640dea2148983ffe490e5bbd1d
rubeen06 <136490926+rubeen06@users.noreply.github.com>
2024-07-20 18:46:46 +0200

    proyecto

---
+++ src/main/java/com/example/gestionHospital/gestionHospital/controllers/DoctorController.java
@@ -0,0 +1,60 @@
+package com.example.gestionHospital.gestionHospital.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.gestionHospital.gestionHospital.dtos.DoctorDTO;
+import com.example.gestionHospital.gestionHospital.services.DoctorService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/doctores")
+@Validated
+@CrossOrigin(origins = "*")
+public class DoctorController {
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @GetMapping
+    public List<DoctorDTO> getAllDoctors() {
+        return doctorService.mostrarDoctores();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DoctorDTO> getDoctorById(@PathVariable Long id) {
+        DoctorDTO doctorDTO = doctorService.buscarDoctorPorId(id);
+        return ResponseEntity.ok(doctorDTO);
+    }
+
+    @PostMapping
+    public ResponseEntity<DoctorDTO> createDoctor(@Valid @RequestBody DoctorDTO doctorDTO) {
+        DoctorDTO createdDoctor = doctorService.aniadirDoctor(doctorDTO);
+        return ResponseEntity.ok(createdDoctor);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<DoctorDTO> updateDoctor(@PathVariable Long id, @Valid @RequestBody DoctorDTO doctorDTO) {
+        DoctorDTO updatedDoctor = doctorService.actualizarDoctor(id, doctorDTO);
+        return ResponseEntity.ok(updatedDoctor);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDoctor(@PathVariable Long id) {
+        doctorService.borrarDoctor(id);
+        return ResponseEntity.noContent().build();
+    }
+}
